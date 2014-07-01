937af4b3cc72315dfdb992f04c65cc1474e7f1ee
omerh <haimomer@gmail.com>
2014-07-01 16:36:36 +0300

    Create smb.rb
    Example how to use sambal

---
+++ smb.rb
@@ -0,0 +1,16 @@
+#this script requiers to have smbclient (linux) or brew install samba (Mac)
+require 'sambal'
+
+targetservers = [ 'serverip', 'serverip']
+targetsmb = 'sahreName'
+smbuser = 'domainuser'
+smbpassword = 'domainpassword'
+domain = 'domainname'
+
+
+targetservers.each do |server|
+	puts "Connection to server #{server}" 
+	client = Sambal::Client.new(domain: domain, host: server , share: targetsmb, user: smbuser, password: smbpassword)
+	client.put('local_file', 'remote_file_name')
+	client.close
+end
